e23ca1ba4c0a48b52814eaefe78112d49a81b12b
GustavoDeveloper1 <devinvestidor02@gmail.com>
2022-04-04 22:53:18 -0300

    criando a primeira função de inicialização

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const inquire = require('inquire');
+const inquire = require('inquirer');
 const chalk = require('chalk');
 
 const fs = require('fs');
 
 
-console.log('executando ');
+// console.log('executando ');
+
+operation()
+
+function operation() {
+
+    inquire.prompt([{
+        type: 'list',
+        name: 'action',
+        mensage: ' O que voce deseja fazer?',
+        choices: [
+            'Criar Conta',
+            'Consultar Saldo',
+            'Depositar',
+            'Sacar',
+            'Sair'
+        ]
+    }]).then((answer)=> {
+        const action = answer['action'];
+
+        console.log(action);
+
+
+    }).catch(err=>console.log(err))
+
+}
